1c7b4aec1f6840574283316740a05637c9a9e016
Ayush Pareek <AYUSHPAREEK1980@GMAIL.COM>
2025-09-05 17:58:48 +0530

    created main.py and inserted a func

---
+++ main.py
@@ -0,0 +1,13 @@
+def random_quote():
+    """
+    Returns a random motivational or fun quote.
+    Author: Ayush Pareek
+    """
+    quotes = [
+        "Keep going, you’re doing great!",
+        "Bug-free code is a myth, but we try anyway!",
+        "Coffee + Code = Happiness",
+        "Every expert was once a beginner.",
+        "Push early, push often!"
+    ]
+    return random.choice(quotes)
